f34d5062dac05a907619d3d4b226c8025a9dae66
IndiaInk10 <indiaink10@gmail.com>
2023-03-06 12:55:56 +0900

    Chapter04 Computation - Sieve
    Aristotle's Sieve to get prime numbers

---
+++ Basic/Basic/exercise_0413_1.cpp
@@ -0,0 +1,27 @@
+// 아리스토테네스의 체 알고리즘을 통한 소수 찾기 프로그램
+
+// 실제 숫자들을 사용하는 것인 아닌 배열과 인덱스를 통한 방법
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+	int numbers[101] = { 0, };
+	numbers[0] = 1, numbers[1] = 1;
+
+	for (int i = 2; i <= 100; i++)
+	{
+		if (numbers[i] == 1) continue;
+
+		for (int j = 2 * i; j <= 100; j += i)
+			numbers[j] = 1;
+	}
+
+	for (int i = 2; i <= 100; i++)
+	{
+		if (numbers[i] == 0)
+			cout << i << " ";
+	}
+
+	return 0;
+}
